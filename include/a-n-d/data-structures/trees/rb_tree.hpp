99a0e2d55b2a1b7433def28c1813c64cf15db516
Nicolas Schmitt <ge93cuy@mytum.de>
2025-06-30 11:16:44 +0200

    Add Red-Black-Tree

---
+++ include/a-n-d/data-structures/trees/rb_tree.hpp
@@ -0,0 +1,175 @@
+#include <cassert>
+#include <cstdint>
+#include <span>
+#include <vector>
+//---------------------------------------------------------------------------
+using std::byte;
+using std::span;
+using std::vector;
+//---------------------------------------------------------------------------
+namespace data_structures::rb_tree {
+//---------------------------------------------------------------------------
+enum class Color : uint8_t { RED, BLACK };
+enum class Direction : uint8_t { LEFT, RIGHT };
+//---------------------------------------------------------------------------
+template <typename KeyT, typename ValueT> struct RedBlackNode {
+  RedBlackNode<KeyT, ValueT> *children[2];
+  RedBlackNode<KeyT, ValueT> *parent;
+  KeyT key;
+  ValueT value;
+  Color color;
+};
+//---------------------------------------------------------------------------
+template <typename KeyT, typename ValueT> class RedBlackTree {
+public:
+  static const uint64_t kNodeAlignment = alignof(RedBlackNode<KeyT, ValueT>);
+  //---------------------------------------------------------------------------
+  /// Default Constructor.
+  RedBlackTree() = delete;
+  //---------------------------------------------------------------------------
+  /// Constructor. Places the RedBlackTree at given location.
+  explicit RedBlackTree(span<byte> buffer) : buffer(buffer) {}
+  //---------------------------------------------------------------------------
+  /// Destructor.
+  ~RedBlackTree() = default;
+  //---------------------------------------------------------------------------
+  RedBlackNode<KeyT, ValueT> *insert(KeyT key, ValueT value) {
+    RedBlackNode<KeyT, ValueT> *node = allocateNode(key, value);
+    //---------------------------------------------------------------------------
+    if (size == 1) {
+      root = node;
+    } else {
+      bool left = false;
+      auto &parent = search(key, left);
+      assert(parent != nullptr);
+      //---------------------------------------------------------------------------
+      node->parent = parent;
+      parent->children[static_cast<uint32_t>(left)] = node;
+      //---------------------------------------------------------------------------
+      if (parent->color == Color::RED) {
+        rotate(node);
+      }
+    }
+    //---------------------------------------------------------------------------
+    return node;
+  }
+  //---------------------------------------------------------------------------
+  ValueT lookup(KeyT key) const {
+    bool left = false;
+    auto &parent = search(key, left);
+    //---------------------------------------------------------------------------
+    return parent->children[static_cast<uint32_t>(left)]->value;
+  }
+
+private:
+  RedBlackNode<KeyT, ValueT> *search(KeyT key, bool &left) {
+    assert(root != nullptr);
+    //---------------------------------------------------------------------------
+    RedBlackNode<KeyT, ValueT> *pred = nullptr;
+    vector<RedBlackNode<KeyT, ValueT> *> stack;
+    stack.push_back(root);
+    //---------------------------------------------------------------------------
+    while (stack.size() > 0) {
+      auto cur = stack.back();
+      stack.pop_back();
+      if (cur->key == key) {
+        left = true;
+        return cur;
+      }
+      if (cur->key < key) {
+        if (cur->left != nullptr)
+          stack.push_back(cur->left);
+        else
+          left = true;
+      } else {
+        if (cur->right != nullptr)
+          stack.push_back(cur->right);
+        else
+          left = false;
+      }
+      pred = cur;
+    }
+    //---------------------------------------------------------------------------
+    return pred;
+  }
+  //---------------------------------------------------------------------------
+  void rotate(RedBlackNode<KeyT, ValueT> *cur) {
+    while (true) {
+      //---------------------------------------------------------------------------
+      assert(cur->color == Color::RED);
+      //---------------------------------------------------------------------------
+      if (cur == root)
+        return;
+      //---------------------------------------------------------------------------
+      assert(cur->parent != nullptr);
+      auto &parent = cur->parent;
+      if (parent == root) {
+        root->color = Color::BLACK;
+        return;
+      }
+      //---------------------------------------------------------------------------
+      assert(cur->parent->parent != nullptr);
+      auto &grandparent = parent->parent;
+      assert(grandparent->color == Color::BLACK);
+      if (parent->color == Color::RED) {
+        auto dir = static_cast<uint8_t>(getDir(parent));
+        auto &aunt = grandparent->children[1 - dir];
+        if (aunt->color == Color::RED) { // Case 2
+          parent->color = Color::BLACK;
+          aunt->color = Color::BLACK;
+          grandparent->color = Color::RED;
+        } else {
+          if (cur == parent->children[1 - dir]) { // Case 5
+            // Rotate
+            parent->children[1 - dir] = cur->children[dir];
+            cur->children[dir] = parent;
+            cur->parent = grandparent;
+            parent->parent = cur;
+            // Reassign
+            auto temp = cur;
+            cur = parent;
+            parent = cur;
+          }
+          // Case 6
+          // Rotate
+          grandparent->children[dir] = parent->children[1 - dir];
+          parent->children[1 - dir] = grandparent;
+          parent->parent = grandparent->parent;
+          grandparent->parent = parent;
+          // Color
+          parent->color = Color::BLACK;
+          grandparent->color = Color::RED;
+        }
+      }
+      //---------------------------------------------------------------------------
+      cur = cur->parent->parent;
+    }
+  }
+  //---------------------------------------------------------------------------q
+  Direction getDir(RedBlackNode<KeyT, ValueT> *node) const {
+    assert(node != nullptr);
+    //---------------------------------------------------------------------------
+    if (node->parent == nullptr || node->parent->children[0] == node)
+      return Direction::LEFT;
+    return Direction::RIGHT;
+  }
+  //---------------------------------------------------------------------------
+  RedBlackNode<KeyT, ValueT> *allocateNode(KeyT key, ValueT value) {
+    uint64_t offset = size * kNodeAlignment;
+    uint64_t aligned_offset =
+        (offset + kNodeAlignment - 1) & ~(kNodeAlignment - 1);
+    //---------------------------------------------------------------------------
+    if (aligned_offset + kNodeAlignment > buffer.size())
+      throw std::bad_alloc();
+    void *node_ptr = buffer.data() + aligned_offset;
+    //---------------------------------------------------------------------------
+    ++size;
+    return new (node_ptr) RedBlackNode({}, nullptr, key, value, Color::RED);
+  }
+
+  span<byte> buffer;
+  RedBlackNode<KeyT, ValueT> *root = nullptr;
+  uint64_t size = 0;
+};
+//---------------------------------------------------------------------------
+} // namespace data_structures::rb_tree
