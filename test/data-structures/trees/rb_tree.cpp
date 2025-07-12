614b9e61b94b8631c90ad148518fdc479b46ebeb
Nicolas Schmitt <ge93cuy@mytum.de>
2025-07-12 17:32:59 +0200

    Add more test coverage

--- test/data-structures/trees/rb_tree.cpp
+++ test/data-structures/trees/rb_tree.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <memory>
+#include <random>
 //---------------------------------------------------------------------------
 #include "trees/rb_tree.hpp"
 //---------------------------------------------------------------------------
@@ -19,8 +20,8 @@ TEST(RBTree, Insert1Get1) {
   ASSERT_EQ(found->value, 2);
 }
 //---------------------------------------------------------------------------
-TEST(RBTree, Insert4Consecutive) {
-  const u32 cinsert = 4;
+TEST(RBTree, ConsecutiveInsert10) {
+  const u32 cinsert = 10;
   //---------------------------------------------------------------------------
   auto buffer = make_unique<byte[]>(1024);
   RedBlackTree<u32, u32> rb(span<byte>(buffer.get(), 1024));
@@ -32,5 +33,59 @@ TEST(RBTree, Insert4Consecutive) {
     auto found = rb.lookup(i);
     ASSERT_NE(found, nullptr);
     ASSERT_EQ(found->value, i * 42);
+  }
+}
+//---------------------------------------------------------------------------
+TEST(RBTree, RandomInsert10) {
+  const u32 cinsert = 10;
+  const u32 seed = 12345;
+  //---------------------------------------------------------------------------
+  auto buffer = make_unique<byte[]>(1024);
+  RedBlackTree<u32, u32> rb(span<byte>(buffer.get(), 1024));
+  //---------------------------------------------------------------------------
+  std::vector<u32> keys(cinsert);
+  std::iota(keys.begin(), keys.end(), 0);
+  std::mt19937 rng(seed);
+  std::shuffle(keys.begin(), keys.end(), rng);
+  //---------------------------------------------------------------------------
+  for (u32 i = 0; i < cinsert; ++i) {
+    u32 key = keys[i];
+    auto node = rb.insert(key, key * 42);
+    ASSERT_NE(node, nullptr);
+  }
+  for (u32 i = 0; i < cinsert; ++i) {
+    u32 key = keys[i];
+    auto found = rb.lookup(key);
+    ASSERT_NE(found, nullptr);
+    ASSERT_EQ(found->value, key * 42);
+  }
+}
+//---------------------------------------------------------------------------
+TEST(RBTree, DuplicateInsert10) {
+  const u32 cinsert = 10;
+  const u32 seed = 12345;
+  //---------------------------------------------------------------------------
+  auto buffer = make_unique<byte[]>(1024);
+  RedBlackTree<u32, u32> rb(span<byte>(buffer.get(), 1024));
+  //---------------------------------------------------------------------------
+  std::vector<u32> keys;
+  keys.reserve(cinsert);
+  std::mt19937 rng(seed);
+  std::uniform_int_distribution<u32> dist(0, cinsert - 1);
+  for (u32 i = 0; i < cinsert; ++i) {
+    keys.push_back(dist(rng));
+  }
+  //---------------------------------------------------------------------------
+  for (u32 i = 0; i < cinsert; ++i) {
+    u32 key = keys[i];
+    auto node = rb.insert(key, key * 42);
+    rb.print();
+    ASSERT_NE(node, nullptr);
+  }
+  for (u32 i = 0; i < cinsert; ++i) {
+    u32 key = keys[i];
+    auto found = rb.lookup(key);
+    ASSERT_NE(found, nullptr);
+    ASSERT_EQ(found->value, key * 42);
   }
 }
